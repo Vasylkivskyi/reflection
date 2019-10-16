d1f40b75e89878f085a26e5060ce5cbe84ebf930
Pavel Vasylkivskiy <pavelvasylkivskiy@Pavels-MacBook-Pro.local>
2019-10-16 22:57:24 +0300

    added comments

--- src/controllers/Helper.js
+++ src/controllers/Helper.js
@@ -2,18 +2,22 @@ import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 
 const Helper = {
+  // Hashing the password to store safe in the db
   hashPassword(password) {
     return bcrypt.hashSync(password, bcrypt.genSaltSync(8));
   },
 
+  // Comparing entered password with user password
   comparePassword(password, hashPassword) {
     return bcrypt.compareSync(password, hashPassword);
   },
 
+  // Validating an email
   isValidEmail(email) {
     return /\S+@\S+\.\S+/.test(email);
   },
 
+  // Generate new token
   generateToken(id) {
     const token = jwt.sign({
       userId: id
