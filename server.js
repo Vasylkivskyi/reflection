c3d2fb2a3717ee74a8034221177544094613f452
Pavel Vasylkivskiy <pavel.vasylkivskiy@gmail.com>
2019-10-19 22:10:21 +0300

    Added user routes

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import express from 'express';
 import dotenv from 'dotenv';
 import 'babel-polyfill';
 import Reflection from './src/controllers/Reflection';
+import Auth from './src/middleware/Auth';
+import User from './src/controllers/User';
 
 dotenv.config();
 console.log("----", Reflection.create);
@@ -13,11 +15,14 @@ app.get('/', (req, res) => {
   return res.status(200).send({ 'message': 'YAY! Congratulations! Your first endpoint is working' });
 });
 
-app.post('/api/v1/reflections', Reflection.create);
-app.get('/api/v1/reflections', Reflection.getAll);
-app.get('/api/v1/reflections/:id', Reflection.getOne);
-app.put('/api/v1/reflections/:id', Reflection.update);
-app.delete('/api/v1/reflections/:id', Reflection.delete);
+app.post('/api/v1/reflections', Auth.verifyToken, Reflection.create);
+app.get('/api/v1/reflections', Auth.verifyToken, Reflection.getAll);
+app.get('/api/v1/reflections/:id', Auth.verifyToken, Reflection.getOne);
+app.put('/api/v1/reflections/:id', Auth.verifyToken, Reflection.update);
+app.delete('/api/v1/reflections/:id', Auth.verifyToken, Reflection.delete);
+app.post('/api/v1/users', User.create);
+app.post('/api/v1/users/login', User.login);
+app.delete('/api/v1/users/me', Auth.verifyToken, User.delete);
 
 const PORT = process.env.PORT || 3000;
 app.listen(3000, () => console.log(`App is running on port ${PORT}`));
